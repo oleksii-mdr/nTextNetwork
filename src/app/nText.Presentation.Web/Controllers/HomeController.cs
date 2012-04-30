652e0d883f532225ac72293de4ffb4db0a3187f5
Oleksii Mandrychenko <oleksii.mdr@gmail.com>
2012-04-30 22:32:42 +0100

    adding presentation project

---
+++ src/app/nText.Presentation.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace nText.Presentation.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/app/nText.Presentation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nText.Presentation.Web.MvcApplication" Language="C#" %>
