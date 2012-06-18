62da877298c54fae89c0fa7e825d1f464e9d9845
unknown <toxa@injetoxa.(none)>
2012-06-18 22:25:59 +0300

    added file upload and json for jit closes #4

---
+++ src/app/nTextNetwork.Core/Impl/ColorHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+
+namespace nTextNetwork.Core.Impl
+{
+   public class ColorHelper
+    {
+        static Random random = new Random();
+
+        public static string GetHexColor()
+        {
+            return ColorTranslator.ToHtml(Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255), 0));
+        }
+    }
+}
